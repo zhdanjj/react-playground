aa850b9a79fcf41c7d063222931803cf82ad0d04
zhdanjj <zhdanjj@gmail.com>
2022-05-26 20:54:37 +0000

    Deploying to gh-pages from  @ e3234641d4771fb6e6b50110f9168be5fc2eea62 🚀

--- src/WeatherForecast/WeatherForecast.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { WeatherForecast } from './WeatherForecast';
-
-export default {
-  title: 'Weather Forecast',
-  component: WeatherForecast,
-}
-
-export const Default = () => <WeatherForecast />;
--- src/WeatherForecast/WeatherForecast.tsx
+++
@@ -1,3 +0,0 @@
-export function WeatherForecast () {
-  return <div>Hello world!</div>;
-}
